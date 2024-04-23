3281f04162270aaf2d1ff76bcb63a15284b4f6a4
Valouche <valentin.arthur1000@gmail.com>
2024-04-23 18:49:25 +0200

    Adding entity compagny

---
+++ src/Form/CompagnyType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Compagny;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompagnyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('logo_path')
+            ->add('adress')
+            ->add('city')
+            ->add('zip_code')
+            ->add('email')
+            ->add('siret')
+            ->add('naf')
+            ->add('website')
+            ->add('phone')
+            ->add('capital')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Compagny::class,
+        ]);
+    }
+}

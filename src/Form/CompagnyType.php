c6bce550cea649e054d96656792921e5cf08a4a2
Valouche <valentin.arthur1000@gmail.com>
2024-04-25 14:22:16 +0200

    Adding constraints on the form of compagny

--- src/Form/CompagnyType.php
+++ src/Form/CompagnyType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -18,39 +19,56 @@ class CompagnyType extends AbstractType
         $builder
             ->add('name', TextType::class, [
                 'label' => 'Nom',
+                'required' => true,
                 'constraints' => [
                     new NotBlank(['message' => 'Le nom est requis.']),
                 ],
             ])
             ->add('logo_path', FileType::class, [
                 'label' => 'Logo',
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'maxSizeMessage' => 'Le fichier est trop volumineux. La taille maximale autorisée est {{ limit }}.',
+                    ]),
+                ],
             ])
             ->add('adress', TextType::class, [
                 'label' => 'Adresse',
+                'required' => false,
             ])
             ->add('city', TextType::class, [
                 'label' => 'Ville',
+                'required' => false,
             ])
             ->add('zip_code', TextType::class, [
                 'label' => 'Code postal',
+                'required' => false,
             ])
             ->add('email', TextType::class, [
                 'label' => 'Email',
+                'required' => false,
             ])
             ->add('siret', TextType::class, [
                 'label' => 'SIRET',
+                'required' => false,
             ])
             ->add('naf', TextType::class, [
                 'label' => 'Code NAF',
+                'required' => false,
             ])
             ->add('website', TextType::class, [
                 'label' => 'Site Web',
+                'required' => false,
             ])
             ->add('phone', TextType::class, [
                 'label' => 'Téléphone',
+                'required' => false,
             ])
             ->add('capital', TextType::class, [
                 'label' => 'Capital',
+                'required' => false,
             ])
         ;
     }

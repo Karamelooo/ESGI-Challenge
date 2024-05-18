c4a4f70aef6b7d63d6d186ae1635df600343d4d4
Valouche <valentin.arthur1000@gmail.com>
2024-05-18 15:32:33 +0200

    feat(subscription): Adding crud

---
+++ src/Form/SubscriptionType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Subscription;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class SubscriptionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('start_date', DateType::class, [
+                'label' => 'Date de début',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'La date de début est requise.']),
+                ],
+            ])
+            ->add('end_date', DateType::class, [
+                'label' => 'Date de fin',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'La date de fin est requise.']),
+                ]
+            ])
+            ->add('compagny_subcription', ChoiceType::class, [
+                'label' => 'Entreprise correspondante',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'La sélection de l\'entreprise est requise.']),
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Subscription::class,
+        ]);
+    }
+}

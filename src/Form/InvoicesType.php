d94c4500ce11a7b7c3ec06ded89a45a246f83362
Léo Guirado <horotopia@hotmail.com>
2024-07-01 20:03:06 +0200

    fix(invoices): form

--- src/Form/InvoicesType.php
+++ src/Form/InvoicesType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Invoices;
+use App\Entity\Client;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class InvoicesType extends AbstractType
@@ -13,10 +16,21 @@ class InvoicesType extends AbstractType
     {
         $builder
             ->add('invoices_number')
-            ->add('client')
+            ->add('client', EntityType::class, [
+                'class' => Client::class,
+                'choice_label' => 'company',
+            ])
             ->add('company')
             ->add('due_date')
-        ;
+            ->add('orders', CollectionType::class, [
+                'entry_type' => OrderType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'by_reference' => false,
+                'prototype' => true,
+                'prototype_name' => '__name__',
+            ])
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
